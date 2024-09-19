734d7f1e1bcd193a76c9ef4df21aa2fec9093a46
Arif2ankhi <arif2ankhi@gmail.com>
2024-09-19 06:21:55 +0100

    Added add money button

--- js/login.js
+++ js/login.js
@@ -5,12 +5,43 @@
 
 // step -1 : set event handler
 
+// document.getElementById('button-login')
+// .addEventListener('click', function(event){
+//     // prevent default behaviour
+//     event.preventDefault();
+// // step3: get the phone number and pin number 
+// const phoneNumber = document.getElementById('phone-number').value;
+// const pinNumber = document.getElementById('pin-number').value;
+// console.log(phoneNumber, pinNumber);
+
+// // step 5 validate phone and pin number
+// if(phoneNumber === '5' && pinNumber ==='1234'){
+//     console.log('you are logged in');
+
+// }
+// else{
+//     alert('wrong phone number or pin')
+// }
+// })
+
 document.getElementById('button-login')
-.addEventListener('click', function(event){
-    // prevent default behaviour
-    event.preventDefault();
-console.log('login button clicked');
-// step3: get the phone number 
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        
+// get phone  number and the pin 
+
 const phoneNumber = document.getElementById('phone-number').value;
-console.log(phoneNumber)
-})
+const pinNumber = document.getElementById('pin-number').value;
+// console.log(phoneNumber, pinNumber)
+// bad way to validate
+if(phoneNumber === '5' && pinNumber ==='1234'){
+    console.log('you are log in');
+    window.location.href = './home.html';
+
+}
+else{
+    alert('Wrong phone number or pin ');
+}
+    
+
+});
